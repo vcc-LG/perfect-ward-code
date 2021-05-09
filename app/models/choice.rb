1c9f3b71132860094ec57747a5d2bce8425ca7ea
Leo Garcia <leogarcia61@gmail.com>
2021-05-09 21:06:54 +0100

    Removed Choice

--- app/models/choice.rb
+++
@@ -1,2 +0,0 @@
-class Choice < ApplicationRecord
-end
--- db/migrate/20210509200338_create_choices.rb
+++
@@ -1,8 +0,0 @@
-class CreateChoices < ActiveRecord::Migration[6.1]
-  def change
-    create_table :choices do |t|
-
-      t.timestamps
-    end
-  end
-end
